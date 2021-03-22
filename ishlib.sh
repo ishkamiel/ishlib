7e5049a4123fef9cda05b3a851c5f12054df6945
Hans Liljestrand <hans@liljestrand.dev>
2021-03-22 16:12:28 -0400

    Update download_file

--- ishlib.sh
+++ ishlib.sh
@@ -223,8 +223,8 @@ fail() {
 
 #------------------------------------------------------------------------------
 : <<'DOCSTRING'
-downloadFile $url $dst
-----------------------
+download_file $url $dst
+-----------------------
 
 Attempts to download file at $url to $dst, creating the containing directory
 if needed. Will first try curl, then wget, and finally fail if neither is
@@ -235,25 +235,36 @@ Arguments:
   dsg - the filename to store the download at
 Returns: 
   0 - on success
-  1 - when download failed
+  1 - bad arguments given
   2 - when neither curl nor wget was found
+  x - error code from curl/wget
 
 DOCSTRING
-downloadFile() {
+download_file() {
     [ -z "$1" ] && warn "downloadFile: bad 1st arg" && return 1
     [ -z "$2" ] && warn "downloadFile: bad 2nd arg" && return 1
 
-    ish_say "downloading ${1} to ${2}"
+    say "downloading ${1} to ${2}"
     mkdir -p "$(dirname "$2")"
 
     if command -v curl >/dev/null 2>&1; then
+      if [ "${DRY_RUN:-0}" = 1 ]; then
+        say "dry_run:" do_o --progress-bar -fLo "$2" --create-dirs "$1"
+        return 0
+      else
         curl --progress-bar -fLo "$2" --create-dirs "$1"
+        return $?
+      fi
     elif command -v wget >/dev/null 2>&1; then
+      if [ "${DRY_RUN:-0}" = 1 ]; then
+        say "dry_run:" wget -nv -O "$2" "$1"
+        return 0
+      else
         wget -nv -O "$2" "$1"
-    else
-        warn "downloadFile: Cannot find curl or wget!" && return 2
+        return $?
+      fi
     fi
-    return 0
+    warn "downloadFile: Cannot find curl or wget!" && return 2
 }
 
 #------------------------------------------------------------------------------
