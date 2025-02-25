a72813d6c69330c73c3799c65eaa321d2674c94e
Hans Liljestrand <hans@liljestrand.dev>
2025-02-25 21:36:41 +0200

    wip

---
+++ src/pyishlib/brew_installer.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Hans Liljestrand <hans@liljestrand.dev>
+# Copyright (C) 2025 Hans Liljestrand <hans@liljestrand.dev>
+#
+# Distributed under terms of the MIT license.
+"""Helper library for package installing tasks"""
+
+import subprocess
+from subprocess import CompletedProcess, CalledProcessError
+from typing import Any, Optional, Iterable
+from .command_runner import CommandRunner
+from .ish_comp import IshComp
+
+
+class BrewInstaller:
+    """Helper class for managing packages via Homebrew"""
+
+    def __init__(self) -> None:
+        self._brew_checked: bool = False
+        self._has_brew: bool = False
+        assert isinstance(self, IshComp)
+        self.log = getattr(self, "log", None)
+        self.runner: CommandRunner = getattr(self, "runner", None)
+
+    @property
+    def has_brew(self) -> bool:
+        """Check if Homebrew is available"""
+        if not self._brew_checked:
+            self._has_brew = self.runner.which("brew") is not None
+            self._brew_checked = True
+        return self._has_brew
+
+    @property
+    def brew(self) -> bool:
+        """Get the common Namespace for installer commands"""
+
+        # pylint: disable=R0903
+        class Namespace:
+            """Namespace for brew commands"""
+
+            can_install = self.can_use_brew
+            install = self.install_brew_pkgs
+            install_unless_found = self.install_brew_pkg_unless_found
+            is_installed = self.is_brew_pkg_installed
+            update = self.update_brew_pkgs
+            update_and_install_all = self.update_and_install_brew_pkgs
+
+        return Namespace
+
+    def can_use_brew(self, pkg: Optional[Any] = None) -> bool:
+        """Check if Homebrew is available, and optionally, if pkg can use it"""
+        if pkg is not None and "brew" not in pkg:
+            return False
+        return self.has_brew
+
+    def get_brew_pkgs(self, pkgs) -> list[dict]:
+        """Get the Homebrew packages from a list of packages"""
+        return [pkg for pkg in pkgs if self.can_use_brew(pkg)]
+
+    def is_brew_pkg_installed(self, pkg) -> bool:
+        """Check if a Homebrew package is installed"""
+        if not self.can_use_brew():
+            self.log.debug("Homebrew not available")
+            return False
+        if not self.can_use_brew(pkg):
+            self.log.debug("Homebrew pkg not available for %s", pkg["name"])
+            return False
+
+        try:
+            result: subprocess.CompletedProcess = self.runner.run(
+                ["brew", "list", "--formula"],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            return pkg["brew"] in result.stdout.decode("utf-8")
+        except CalledProcessError as e:
+            self.log.critical("Homebrew error checking %s: %s", pkg["name"], e)
+            raise e
+
+    def install_brew_pkgs(self, pkgs: Iterable[dict]) -> bool:
+        """Install a list of Homebrew packages"""
+        assert isinstance(pkgs, Iterable) and all(
+            isinstance(pkg, dict) for pkg in pkgs
+        ), "pkgs should be an iterable of dictionaries"
+        assert all(self.can_use_brew(p) for p in pkgs)
+
+        pkg_list: list[str] = [pkg["brew"] for pkg in pkgs]
+
+        self.log.info("Installing with Homebrew: %s", " ".join(pkg_list))
+        try:
+            res: CompletedProcess = self.runner.run(["brew", "install"] + pkg_list)
+            return res.returncode == 0
+        except CalledProcessError as e:
+            self.log.critical("Homebrew error installing %s: %s", " ".join(pkg_list), e)
+            raise e
+
+    def install_brew_pkg(self, pkg) -> bool:
+        """Install a Homebrew package"""
+        self.install_brew_pkgs([pkg])
+
+    def install_brew_pkg_unless_found(self, pkg) -> bool:
+        """Install a Homebrew package unless it is already installed"""
+        if not self.is_brew_pkg_installed(pkg):
+            self.install_brew_pkg(pkg)
+
+    def update_brew_pkgs(self) -> bool:
+        """Update all installed Homebrew packages"""
+        assert self.can_use_brew()
+
+        self.runner.run(["brew", "update"])
+        self.runner.run(["brew", "upgrade"])
+
+    def update_and_install_brew_pkgs(self, pkgs):
+        """Update Homebrew and Homebrew packages, then install new Homebrew pkgs"""
+        assert self.can_use_brew()
+
+        self.update_brew_pkgs()
+        self.install_brew_pkgs(pkgs)
---
+++ src/pyishlib/pip_installer.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Hans Liljestrand <hans@liljestrand.dev>
+# Copyright (C) 2025 Hans Liljestrand <hans@liljestrand.dev>
+#
+# Distributed under terms of the MIT license.
+"""Helper library for package installing tasks"""
+
+import subprocess
+from subprocess import CompletedProcess, CalledProcessError
+from typing import Any, Optional, Iterable
+from .command_runner import CommandRunner
+from .ish_comp import IshComp
+
+
+class PipInstaller:
+    """Helper class for managing python packages via pip"""
+
+    PIP_UPDATE_PKG: dict[str, str] = {
+        "name": "pip",
+        "pip": "pip",
+    }
+
+    PIP_INSTALL_CMD: list[str] = ["pip3", "install", "--user"]
+
+    def __init__(self) -> None:
+        self._pip_checked: bool = False
+        self._has_pip: bool = False
+        assert isinstance(self, IshComp)
+        self.log = getattr(self, "log", None)
+        self.runner: CommandRunner = getattr(self, "runner", None)
+
+    @property
+    def has_pip(self) -> bool:
+        """Check if pip is available"""
+
+        if not self._pip_checked:
+            self._has_pip = self.runner.which("pip3") is not None
+            self._pip_checked = True
+        return self._has_pip
+
+    @property
+    def pip(self) -> bool:
+        """Get the common Namespace for installer commands"""
+
+        # pylint: disable=R0903
+        class Namespace:
+            """Namespace for pip commands"""
+
+            can_install = self.can_use_pip
+            install = self.install_pip_pkgs
+            install_unless_found = self.install_pip_pkg_unless_found
+            is_installed = self.is_pip_pkg_installed
+            update = self.update_pip_pkgs
+            update_and_install_all = self.update_and_install_pip_pkgs
+
+        return Namespace
+
+    def can_use_pip(self, pkg: Optional[Any] = None) -> bool:
+        """Check if pip is available, and optionally, if pkg can use it"""
+
+        if pkg is not None and "pip" not in pkg:
+            return False
+        return self.has_pip
+
+    def get_pip_pkgs(self, pkgs) -> list[dict]:
+        """Get the pip packages from a list of packages"""
+        return [pkg for pkg in pkgs if self.can_use_pip(pkg)]
+
+    def is_pip_pkg_installed(self, pkg) -> bool:
+        """Check if a pip package is installed"""
+
+        if not self.can_use_pip():
+            self.log.debug("Pip not available")
+            return False
+        if not self.can_use_pip(pkg):
+            self.log.debug("Pip pkg not available for %s", pkg["name"])
+            return False
+
+        try:
+            result: subprocess.CompletedProcess = self.runner.run(
+                ["pip3", "list"],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            return pkg["pip"] in result.stdout.decode("utf-8")
+        except CalledProcessError as e:
+            self.log.critical("Pip error checking %s: %s", pkg["name"], e)
+            raise e
+
+    def install_pip_pkgs(self, pkgs: Iterable[dict]) -> bool:
+        """Install a list of pip packages"""
+
+        assert isinstance(pkgs, Iterable) and all(
+            isinstance(pkg, dict) for pkg in pkgs
+        ), "pkgs should be an iterable of dictionaries"
+        assert all(self.can_use_pip(p) for p in pkgs)
+
+        pkg_list: list[str] = [pkg["pip"] for pkg in pkgs]
+
+        self.log.info("Installing with pip: %s", " ".join(pkg_list))
+        try:
+            res: CompletedProcess = self.runner.run(self.PIP_INSTALL_CMD + pkg_list)
+            return res.returncode == 0
+        except CalledProcessError as e:
+            self.log.critical("Pip error installing %s: %s", " ".join(pkg_list), e)
+            raise e
+
+    def install_pip_pkg(self, pkg) -> bool:
+        """Install a pip package"""
+        self.install_pip_pkgs([pkg])
+
+    def install_pip_pkg_unless_found(self, pkg) -> bool:
+        """Install a pip package unless it is already installed"""
+
+        if not self.is_pip_pkg_installed(pkg):
+            self.install_pip_pkg(pkg)
+
+    def update_pip_pkgs(self) -> bool:
+        """Update all installed pip packages"""
+        assert self.can_use_pip()
+
+        self.install_pip_pkg_unless_found(self.PIP_UPDATE_PKG)
+        self.runner.run(["pip3", "install", "--upgrade", "pip"])
+        self.log.warning("pip update not implemented (only updates pip itself)")
+
+    def update_and_install_pip_pkgs(self, pkgs):
+        """Update python, pip and pip packages, then install new pip pkgs"""
+        assert self.can_use_pip()
+
+        self.update_pip_pkgs()
+        self.install_pip_pkgs(pkgs)
